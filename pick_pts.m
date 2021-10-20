d6038cd2a730ff76816eca0e2f988b85464be2f0
AWESOMESAUCE170 <55606249+AWESOMESAUCE170@users.noreply.github.com>
2021-10-20 12:48:49 -0500

    Copied pick_pts from lecture

---
+++ pick_pts.m
@@ -0,0 +1,9 @@
+function [pixels] = pick_pts(img)
+imshow(img);
+hold on;
+for i = 1 : 6
+    [x,y] = ginput(1);
+    plot(x,y, '+', 'color', 'y', 'marksize', 10);
+    pixels(1, i) = x;
+    pixels(2, i) = y;
+end
