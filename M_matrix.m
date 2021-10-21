34bd35b9eb8bf4ca968916f799a00494c466759b
AWESOMESAUCE170 <55606249+AWESOMESAUCE170@users.noreply.github.com>
2021-10-21 16:50:54 -0500

    Copied M_matrix from lecture

---
+++ M_matrix.m
@@ -0,0 +1,12 @@
+function [M] = M-matrix(pixels, X)
+%
+num = size(pixels, 2);
+pixels = [pixels; ones(1,num)];
+X = [X; ones(1,num)];
+M = zeros(2*num, 12);
+for i = 1 : num
+    M(2*i-1, 1:4) = X(:,i)';
+    M(2*i-1, 9:12) = -X(:,i)'*pixels(1,i);
+    M(2*i, 5:8) = -X(:,i)';
+    M(2*i, 9:12) = -X(:,i)'*pixels(2,i);
+end
